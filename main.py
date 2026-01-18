06a8254f51895b1556c3a79b014ea855621b35d8
Yeamem Hassan <yeaminhassan2002@gmail.com>
2026-01-18 21:54:54 +0400

    Add expense and view expense functionally

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
+expenses = []
+
 def show_menu():
     print("\n=== Expense Tracker ===")
     print("1. Add expense")
     print("2. View expense")
     print("3. Exit")
+
+def add_expense():
+    amount = input("Enter Amount:")
+    category = input("Enter Category:")
+    note = input("Enter Note (optional):")
+    
+    expense = {
+        "amount": amount,
+        "category": category,
+        "note": note
+    }
+    
+    expenses.append(expense)
+    print("Expense added successfully!")
+    
+def view_expenses():
+    if not expenses:
+        print("No expenses recorded.")
+        return
+    
+    print("\n--- Expenses ---")
+    for i, expense in enumerate(expenses, start=1):
+        print(
+            f"{i}. Amount: {expense['amount']} |"
+            f" Category: {expense['category']} |"
+            f" Note: {expense['note']}"
+        )
     
 def main():
     while True:
@@ -10,9 +39,9 @@ def main():
         choice = input("Choose an option (1-3): ")
         
         if choice == "1":
-            print("Add expense selected (not implemented yet)")
+            add_expense()
         elif choice == "2":
-            print("View expense selected (not implemented yet)")
+            view_expenses()
         elif choice == "3":
             print("Exiting the program. GoodBye!")
             break
